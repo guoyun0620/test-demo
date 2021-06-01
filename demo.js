93bc7899863dc0f20f339b19c0c249743cd56a07
郭赟 <m18234097683_1@163.com>
2021-06-01 21:11:40 +0800

    2222

--- demo.js
+++ demo.js
@@ -4,5 +4,8 @@ window.onload=function(){
     box.addEventListener('click',() => {
         alert("这是一ggggg个测试代码");
         alert('冲突2')
+        alert('冲突3')
+        alert("冲突33333")
+        alert("冲突33dddd333")
     })
 }
