9fc3767f9b73ef3a0cd875b3d1c04386b9611c09
郭赟 <m18234097683_1@163.com>
2021-06-01 21:17:35 +0800

    3333

--- demo.js
+++ demo.js
@@ -3,6 +3,7 @@ window.onload=function(){
     let box=document.getElementsByClassName('box')[0];
     box.addEventListener('click',() => {
         alert("这是一ggggg个测试代码");
+        alert("冲突1")
         alert('冲突2')
     })
 }
